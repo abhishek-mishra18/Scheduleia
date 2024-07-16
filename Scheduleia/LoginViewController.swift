049da808f084ec2ea00dc307498e7ce0a0812120
Abhishek Mishra <abhishek.mishra@zopsmart.com>
2024-07-16 13:21:39 +0530

    First Commit

---
+++ Scheduleia/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  Scheduleia
+//
+//  Created by Abhishek on 16/07/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Scheduleia/ViewController.swift
+++ Scheduleia/ViewController.swift
@@ -8,9 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let titleText = "📋SCHEDULEIA"
 
+    @IBOutlet weak var appName: UILabel!
+    
+    @IBAction func LoginTapped(_ sender: Any) {
+    }
+    
+    @IBAction func RegisterTapped(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var index = 0.0
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * index, repeats: false, block: {(timer) in
+                self.appName.text?.append(letter)
+            })
+            index += 1
+        }
+
+
+
+
         // Do any additional setup after loading the view.
     }
 
