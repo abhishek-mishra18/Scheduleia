c980116a8b911bd142d94a94e1c5712ac2605457
Abhishek Mishra <abhishek.mishra@zopsmart.com>
2024-07-17 13:00:47 +0530

    Login added

--- Scheduleia/LoginViewController.swift
+++ Scheduleia/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
@@ -16,6 +17,22 @@ class LoginViewController: UIViewController {
     
     
     @IBAction func loginButtonTapped(_ sender: Any) {
+        if let email = userNameTextField.text, let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult,error in
+                if let e = error {
+                    print(e)
+                    let alert = UIAlertController(title: "Error occured", message: e.localizedDescription, preferredStyle: .alert)
+                    let action = UIAlertAction(title: "okay", style: .cancel, handler: {_ in
+                        self?.userNameTextField.text = ""
+                        self?.passwordTextField.text = ""
+                    })
+                    alert.addAction(action)
+                    self?.present(alert, animated: true)
+                }else{
+                    self?.performSegue(withIdentifier: "loginSegue", sender: self)
+                }
+                }
+        }
     }
     
     override func viewDidLoad() {
